2be65a2ae0f7c13a98ca103e2a8a7729abbac262
noor7599 <126394883+noor7599@users.noreply.github.com>
2023-02-27 09:50:07 -0800

    Add files via upload

---
+++ Testdb.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE testdb;
+CREATE DATABASE IF NOT EXISTS textdb;
+
+CREATE TABLE persons(
+personID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar (255),
+City varchar(225)
+);
